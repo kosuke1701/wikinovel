a1df58d44d9526b305d9fc2f1f9eac5c679f890d
Kosuke Akimoto <kosuke1701@gmail.com>
2023-03-02 20:52:20 +0900

    Add select wiki section chain.

--- src/wikinovel/wiki.py
+++ src/wikinovel/wiki.py
@@ -7,7 +7,7 @@ https://opensource.org/licenses/mit-license.php
 import os
 import re
 
-from langchain.chains import LLMChain, TransformChain, SimpleSequentialChain
+from langchain.chains import LLMChain, TransformChain, SimpleSequentialChain, SequentialChain
 from langchain.llms import OpenAI
 from langchain.prompts import load_prompt
 
@@ -55,5 +55,59 @@ def load_entity_alias_chain(
     
     parse_chain = TransformChain(input_variables=["text"], output_variables=["alias_names"], transform=parse_func)
     chain = SimpleSequentialChain(chains=[chain, parse_chain], input_key="entity")
+
+    return chain
+
+def load_select_wiki_section_chain(
+    llm=None,
+    prompt_fn=DEFAULT_ROOT_DIR + "prompts/select_wiki_section_prompt.json"
+):
+    if llm is None:
+        llm = OpenAI(max_tokens=1024)
+    prompt = load_prompt(prompt_fn)
+    chain = LLMChain(prompt=prompt, llm=llm)
+
+    def func_parse(inputs):
+        text = inputs["text"]
+        parts = {}
+        sections = {}
+        for line in text.split("\n"):
+            m = re.search(r"Section (?P<idx>[0-9]+): (?P<names>.+)", line)
+            if m is not None:
+                idx = m.group("idx")
+                names = m.group("names")
+                names = names.split(" / ")
+                sects = []
+                for name in names:
+                    m = re.search(r"(?P<sec_idx>[0-9.]+) (?P<name>.+)", name)
+                    if m is not None:
+                        sec_idx = m.group("sec_idx")
+                        name = m.group("name")
+                        sec_idx = [int(num) for num in sec_idx.split(".") if len(num) > 0]
+                        sects.append((sec_idx, name))
+                sections[idx] = sects
+            
+                continue
+
+            m = re.search(r"Part (?P<idx>[0-9]+): (?P<content>.+)", line)
+            if m is not None:
+                idx = m.group("idx")
+                content = m.group("content")
+
+                parts[idx] = content
+        
+        outputs = []
+        for idx in parts.keys() & sections.keys():
+            outputs.append((sections[idx], parts[idx]))
+        
+        return {"sections": outputs}
+
+    parse_chain = TransformChain(transform=func_parse, input_variables=["text"], output_variables=["sections"])
+
+    chain = SequentialChain(
+        chains=[chain, parse_chain],
+        input_variables=chain.input_keys,
+        output_variables=["sections"]
+    )
 
     return chain
